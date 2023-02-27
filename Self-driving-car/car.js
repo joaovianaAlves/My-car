f7c6ced12462cf57a1ca0ce8c0f10fcb91a5951a
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-02-27 08:24:14 -0300

    Add files via upload

--- Self-driving-car/car.js
+++ Self-driving-car/car.js
@@ -11,11 +11,13 @@ class Car{
         this.friction=0.05;
         this.angle=0;
 
+        this.sensor=new Sensor(this);
         this.controls=new Controls();
     }
 
-    update(){
+    update(roadBorders){
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move(){
@@ -72,5 +74,7 @@ class Car{
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
